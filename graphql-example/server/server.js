a8b9bb6428f0bbac446a984ab3cb2f5d81bf7c59
luna215 <paulluna0215@gmail.com>
2019-08-16 11:58:25 -0700

    Added UI requests

---
+++ graphql-example/server/server.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require('apollo-server');
+const cors = require('cors');
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+    pages: 1231321
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+    pages: 1
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+    pages: Int
+  }
+  type Query {
+    getBooks: [Book]
+    hello: String
+  }
+`;
+
+// Resolvers handle where the data comes from
+const resolvers = {
+  Query: {
+    getBooks: () => books,
+    hello: () => {
+      return 'Hello, world'
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers, cors: true });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+})
---
+++ graphql-example/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h3>Example 1</h3>
+    <div>
+      Data: {{ example1 }}
+    </div>
+    <button @click="getData">Get Data</button>
+    <hr>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      example1: ''
+    }
+  },
+  methods: {
+    async getData () {
+      try {
+        const res = await axios.post('http://localhost:4000/', {
+          query: '{ hello }'
+        })
+        this.example1 = res.data.data.hello
+      } catch (error) {
+        return;
+        // console.log('err', error);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
